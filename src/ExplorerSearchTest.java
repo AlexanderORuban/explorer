d4e6012f47331aee72dba2368e847f6155aae0b1
Alexander Ruban <alexruban29@gmail.com>
2025-04-07 12:28:16 -0700

    Added tests for explorerLocation method.

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -17,4 +17,60 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+    // ExplorerLocation tests:
+    @Test
+    public void testSalamanderLocation_centerOfGrid() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,0,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        int[] expected = {2, 2};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testSalamanderLocation_RightCornerGrid() {
+        int[][] island = {
+            {1,1,1,1,1,0},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        int[] expected = {0, 5};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testSalamanderLocation_SurroundedByWater() {
+        int[][] island = {
+            {2,2,2,2,2,2},
+            {2,2,2,2,2,2},
+            {2,2,0,2,2,2},
+            {2,2,2,2,2,2},
+            {2,2,2,2,2,2},
+        };
+        int[] expected = {2, 2};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testSalamanderLocation_throwsException() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.explorerLocation(island);
+        });
+        assertEquals("No explorer is present", exception.getMessage());
+    }
+
 }
